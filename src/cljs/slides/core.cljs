ee0820102ce4fb3ec7eebf51d0bb597b19f56e81
Chandu Tennety <chandu.tennety@gmail.com>
2015-04-10 11:40:17 -0400

    render multiple slides

--- src/cljs/slides/core.cljs
+++ src/cljs/slides/core.cljs
@@ -2,30 +2,32 @@
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]))
 
-(defonce app-state (atom {:slide {}}))
+(defonce app-state (atom {:slides []}))
 
 (defn slide [model owner]
   (reify
     om/IRender
     (render [_]
       (dom/section #js {:className "slide"}
-        (when-let [bg (:bg model)]
-          (dom/img #js {:className "bg" :src bg}))
-        (dom/div #js {:className "slide-content"}
-           (dom/h1 #js {} (:title model)))))))
+                   (when-let [bg (:bg model)]
+                     (dom/img #js {:className "bg" :src bg}))
+                   (dom/div #js {:className "slide-content"}
+                            (dom/h1 #js {} (:title model)))))))
 
 (defn app [model owner]
   (reify
-        om/IRender
-        (render [_]
-          (dom/div #js {:className "content"}
-                   (om/build slide (:slide model))))))
+    om/IRender
+    (render [_]
+      (apply dom/div #js {:className "content"}
+               (om/build-all slide (:slides model))))))
 
-(def slide-img
-  {:title "A Bird's Eye View of ClojureScript"
-   :bg "/images/gull.jpg"})
+(def slide-imgs
+  [{:title "A Bird's Eye View of ClojureScript"
+    :bg "/images/gull.jpg"}
+   {:title "Using Om Components"
+    :bg "/images/gull2.jpg"}])
 
 (defn main []
   (om/root app app-state {:target (. js/document (getElementById "app"))}))
 
-(swap! app-state assoc :slide slide-img)
+(swap! app-state assoc :slides slide-imgs)
