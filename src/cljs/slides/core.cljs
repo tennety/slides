29b2c278e8ad4ceffc3b9205f37a669a7423f09b
Chandu Tennety <chandu.tennety@gmail.com>
2015-05-15 16:22:37 -0400

    First slide

--- src/cljs/slides/core.cljs
+++ src/cljs/slides/core.cljs
@@ -2,16 +2,29 @@
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]))
 
-(defonce app-state (atom {:text "Chestnut"}))
+(defonce app-state (atom {:slide {}}))
+
+(defn slide [model owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/section #js {:className "slide"}
+        (when-let [bg (:bg model)]
+          (dom/img #js {:className "bg" :src bg}))
+        (dom/div #js {:className "slide-content"}
+           (dom/h1 #js {} (:title model)))))))
+
+(defn app [model owner]
+  (reify
+    om/IRender
+    (render [_]
+      (om/build slide (:slide model)))))
+
+(def slide-img
+  {:title "A Bird's Eye View of ClojureScript"
+   :bg "/images/gull.jpg"})
 
 (defn main []
-  (om/root
-    (fn [app owner]
-      (reify
-        om/IRender
-        (render [_]
-          (dom/h1 nil (str "Hello " (:text app) "!")))))
-    app-state
-    {:target (. js/document (getElementById "app"))}))
+  (om/root app app-state {:target (. js/document (getElementById "app"))}))
 
-;; (swap! app-state assoc :text "LambdaConf 2015")
+(swap! app-state assoc :slide slide-img)
